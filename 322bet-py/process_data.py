9611089c01996125e814317f13a310dd19f1166e
milanjrodd <milanrodd@gmail.com>
2023-12-13 18:16:39 +0300

    feat: data process script

---
+++ 322bet-py/process_data.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+from parallel_pandas import ParallelPandas
+
+# initialize parallel-pandas
+ParallelPandas.initialize(n_cpu=8, split_factor=4, disable_pr_bar=False)
+
+def process_data():
+  # Get the current working directory
+  data_parser_dir = os.path.join(os.getcwd(), 'dataParser')
+  project_dir = os.path.join(os.getcwd(), '322bet-py')
+
+  # Construct the file paths
+  matches_path = os.path.join(data_parser_dir, 'matches.csv')
+  heroes_path = os.path.join(data_parser_dir, 'heroes.json')
+  
+  # Read the CSV file
+  matches = pd.read_csv(matches_path)
+  heroes = pd.read_json(heroes_path).set_index('id')
+
+  df = pd.DataFrame(columns=['match_id', 'radiant_win', 'radiant_hero_1', 'radiant_hero_2', 'radiant_hero_3', 'radiant_hero_4', 'radiant_hero_5', 'dire_hero_1', 'dire_hero_2', 'dire_hero_3', 'dire_hero_4', 'dire_hero_5', 'avg_rank_tier', 'radiant_hero_1_winrate', 'radiant_hero_2_winrate', 'radiant_hero_3_winrate', 'radiant_hero_4_winrate', 'radiant_hero_5_winrate', 'dire_hero_1_winrate', 'dire_hero_2_winrate', 'dire_hero_3_winrate', 'dire_hero_4_winrate', 'dire_hero_5_winrate'])
+
+  # Add data to the dataframe using pandas library
+
+  df['match_id'] = matches['match_id']
+  df['avg_rank_tier'] = matches['avg_rank_tier']
+  df['radiant_win'] = matches['radiant_win'] + 0
+
+  for i in range(1, 6):
+      df[f'radiant_hero_{i}'] = matches['radiant_team'].str.split(',').str[i-1]
+      df[f'dire_hero_{i}'] = matches['dire_team'].str.split(',').str[i-1]
+
+  # Picks and wins are stored in heroes.json file in colums 1_pick - 8_pick and 1_win - 8_win accordinally. Need to divide wins by picks to get winrate, and then add it to the dataframe
+
+  for i in range(1, 6):
+      df[f'radiant_hero_{i}_winrate'] = df[f'radiant_hero_{i}'].p_apply(lambda x: heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_win'] / heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_pick'])
+      df[f'dire_hero_{i}_winrate'] = df[f'dire_hero_{i}'].p_apply(lambda x: heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_win'] / heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_pick'])
+
+  print(df.head())
+  # Save the dataframe to a CSV file
+  df.to_csv(os.path.join(project_dir, 'data', 'matches_processed.csv'), index=False)
+
+if __name__ == "__main__":
+    process_data()
