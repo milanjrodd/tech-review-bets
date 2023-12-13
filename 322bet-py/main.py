490ee773ee1113b57ee15ca009a6406812445046
milanjrodd <milanrodd@gmail.com>
2023-12-13 10:09:02 +0300

    feat: v0.1 py

---
+++ 322bet-py/main.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import torch
+import numpy
+import matplotlib.pyplot as plt
+from torchsummary import summary
+from datetime import datetime
+from parallel_pandas import ParallelPandas
+from pathlib import Path
+
+# initialize parallel-pandas
+ParallelPandas.initialize(n_cpu=12, split_factor=4, disable_pr_bar=False)
+
+
+def main():
+    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cpu'
+    tensor_device = torch.device(device)
+    # Read mathes data
+
+    heroes = pd.read_json("../dataParser/heroes.json").set_index("id")
+
+    def getHeroWinrate(heroId: int, rank: int) -> float:
+        heroPicks, heroWins = heroes.loc[heroId][[
+            f'{rank}_pick', f'{rank}_win']]
+        heroWinRate = heroWins / heroPicks
+        return round(heroWinRate, 5)
+
+    def mapHeroIdsToWinrates(ids: str, rank: int) -> list[float]:
+        return list([getHeroWinrate(heroId=int(id), rank=rank) for id in ids.split(',')])
+
+    matches = pd.read_csv("../dataParser/matches.csv").head(1500)
+
+    # Normalize and merge data
+    matches['radiant_win'] += 0
+    matches['radiant_heroes_winrates'] = matches.p_apply(lambda x: mapHeroIdsToWinrates(
+        x['radiant_team'], x['avg_rank_tier']//10), axis=1)
+    matches['dire_heroes_winrates'] = matches.p_apply(lambda x: mapHeroIdsToWinrates(
+        x['dire_team'], x['avg_rank_tier']//10), axis=1)
+
+    testDataCount = len(matches)//4
+    trainData = matches.head(-testDataCount)
+    testData = matches.tail(testDataCount)
+
+    data_train_input = (trainData['radiant_heroes_winrates'] +
+                        trainData['dire_heroes_winrates']).to_numpy()
+    data_train_output = [[i] for i in trainData['radiant_win'].to_numpy()]
+
+    # Convert to tensor
+    tensor_train_input = torch.tensor(list(data_train_input)).to(
+        torch.float32).to(tensor_device)
+    tensor_train_output = torch.tensor(list(data_train_output)).to(
+        torch.float32).to(tensor_device)
+
+    print("Input:\n", tensor_train_input)
+    print("Shape:\n", tensor_train_input.shape)
+    print("Answers:\n", tensor_train_output)
+    print("Shape:\n", tensor_train_output.shape)
+
+    # Step 2. Create model
+
+    input_shape = 10
+    output_shape = 1
+    batch_size = 15000
+
+    model = torch.nn.Sequential(
+        torch.nn.Linear(input_shape, 10),
+        torch.nn.ReLU(),
+        torch.nn.Linear(10, 2),
+        torch.nn.ReLU(),
+        torch.nn.Linear(2, output_shape)
+    ).to(tensor_device)
+
+    summary(model, (input_shape,), batch_size=batch_size, device=device)
+
+    # Adam optimizer
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=1e-3, betas=(0.9, 0.99))
+
+    # mean squared error
+    loss = torch.nn.BCELoss()
+
+    # Epochs
+    epochs = 500
+
+    # Step 3. Train model
+    print("Start train")
+
+    history = []
+
+    model = model.to(tensor_device)
+
+    trained = False
+
+    for epoch in range(epochs):
+        if (trained):
+            break
+
+        permutation = torch.randperm(tensor_train_input.size()[0])
+
+        for i in range(0, tensor_train_input.size()[0], batch_size):
+            optimizer.zero_grad()
+
+            indices = permutation[i:i+batch_size]
+            batch_x, batch_y = tensor_train_input[indices], tensor_train_output[indices]
+
+            # in case you wanted a semi-full example
+            outputs = model(batch_x)
+            loss_value = loss(outputs, batch_y)
+
+            # Append to history
+            loss_value_item = loss_value.item()
+            history.append(loss_value_item)
+
+            loss_value.backward()
+            optimizer.step()
+
+            # Break if trained
+            if loss_value_item < 0.001:
+                trained = True
+                break
+
+            # Debug output
+            if epoch % 100 == 0:
+                print(
+                    f"{datetime.now().time()} {epoch+1},\t loss: {loss_value_item:.7f}")
+            if device == 'cuda':
+                torch.cuda.empty_cache()
+
+    # Step 4. Control nn
+    print("Control")
+
+    data_test_input = (testData['radiant_heroes_winrates'] +
+                       testData['dire_heroes_winrates']).to_numpy()
+    data_test_output = testData['radiant_win'].to_numpy()
+
+    tensor_train_input = torch.tensor(list(data_test_input)).to(
+        torch.float32).to(tensor_device)
+
+    print("Shape:", tensor_train_input.shape)
+
+    answer = model(tensor_train_input)
+
+    errors = 0
+
+    for i in range(len(answer)):
+        predicted = int(answer[i].round().item())
+        real = data_test_output.item(i)
+        if predicted != real:
+            errors += 1
+
+    print('Success rate: ', (1-(errors/testDataCount))*100, '%')
+    plt.plot(history)
+    plt.title('Loss')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 322bet-py/xor.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+
+##
+# Copyright (с) Ildar Bikmamatov 2022
+# License: MIT
+# Source: https://github.com/bayrell-tutorials/perceptron-xor
+# Link:
+# https://blog.bayrell.org/ru/iskusstvennyj-intellekt/411-obuchenie-mnogoslojnogo-perseptrona-operaczii-xor.html
+##
+
+import matplotlib.pyplot as plt
+import torch
+import os
+import sys
+from datetime import datetime as dt
+from torch import nn
+from torchsummary import summary
+
+# Detect device for tensor
+# cpu or cuda
+device = 'cuda'
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+tensor_device = torch.device(device)
+
+
+# Step 1. Prepare DataSet
+data_train = [
+    {"in": [0, 0], "out": [0]},
+    {"in": [0, 1], "out": [1]},
+    {"in": [1, 0], "out": [1]},
+    {"in": [1, 1], "out": [0]},
+]
+
+
+# Convert to question and answer
+tensor_train_x = list(map(lambda item: item["in"], data_train))
+tensor_train_y = list(map(lambda item: item["out"], data_train))
+
+# Convert to tensor
+tensor_train_x = torch.tensor(tensor_train_x).to(
+    torch.float32).to(tensor_device)
+tensor_train_y = torch.tensor(tensor_train_y).to(
+    torch.float32).to(tensor_device)
+
+print("Input:\n", tensor_train_x)
+print("Shape:\n", tensor_train_x.shape)
+print("Answers:\n", tensor_train_y)
+print("Shape:\n", tensor_train_y.shape)
+
+# Step 2. Create model
+
+input_shape = 2
+output_shape = 1
+
+model = nn.Sequential(
+    nn.Linear(input_shape, 16),
+    nn.ReLU(),
+    nn.Linear(16, output_shape),
+    # nn.Softmax()
+).to(tensor_device)
+
+summary(model, (input_shape,), device=device)
+
+
+# Adam optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, betas=(0.9, 0.99))
+
+# mean squared error
+loss = nn.MSELoss()
+
+# Batch size
+batch_size = 2
+
+# Epochs
+epochs = 100000
+
+
+# Step 3. Train model
+print("Start train")
+
+history = []
+
+model = model.to(tensor_device)
+
+for i in range(epochs):
+
+    # Model result
+    model_res = model(tensor_train_x)
+
+    # Loss value
+    loss_value = loss(model_res, tensor_train_y)
+
+    # Append to history
+    loss_value_item = loss_value.item()
+    history.append(loss_value_item)
+
+    # Calc gradient
+    optimizer.zero_grad()
+    loss_value.backward()
+
+    # Optimize
+    optimizer.step()
+
+    # Break if trained
+    if loss_value_item < 0.001:
+        break
+
+    # Debug output
+    if i % 100 == 0:
+        print(f"{dt.now().time()} {i+1},\t loss: {loss_value_item:.7f}")
+
+    if device == 'cuda':
+        torch.cuda.empty_cache()
+
+
+# Show history
+
+
+plt.plot(history)
+plt.title('Loss')
+plt.savefig('xor_torch.png')
+plt.show()
+
+
+# Step 4. Control nn
+print("Control")
+
+control_x = [
+    [0, 0],
+    [0, 1],
+    [1, 0],
+    [1, 1],
+]
+
+control_x = torch.tensor(control_x).to(torch.float32).to(tensor_device)
+
+print("Shape:", control_x.shape)
+
+answer = model(control_x)
+
+for i in range(len(answer)):
+    print(control_x[i].tolist(), "->", answer[i].round().tolist())
