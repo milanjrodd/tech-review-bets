9e1d457054691ae77fb3c192849ed8116bc1bb9b
milanjrodd <milanrodd@gmail.com>
2023-12-16 19:41:30 +0300

    refactor for cuda processing

--- 322bet-py/process_data.py
+++ 322bet-py/process_data.py
@@ -1,41 +1,47 @@
 import os
 import pandas as pd
+import pathlib
 from parallel_pandas import ParallelPandas
 
 # initialize parallel-pandas
 ParallelPandas.initialize(n_cpu=8, split_factor=4, disable_pr_bar=False)
 
-def process_matches(path: str):
-  # Get the current working directory
-  data_parser_dir = os.path.join(os.getcwd(), 'dataParser')
 
-  # Construct the file paths
-  matches_path = os.path.join(data_parser_dir, 'matches.csv')
-  heroes_path = os.path.join(data_parser_dir, 'heroes.json')
-  
-  # Read the CSV file
-  matches = pd.read_csv(matches_path).head(160000)
-  heroes = pd.read_json(heroes_path).set_index('id')
+def process_matches(path: str | pathlib.Path):
+    # Get the current working directory
+    data_parser_dir = pathlib.Path("../dataParser")
 
-  df = pd.DataFrame(columns=['match_id', 'radiant_win', 'radiant_hero_1', 'radiant_hero_2', 'radiant_hero_3', 'radiant_hero_4', 'radiant_hero_5', 'dire_hero_1', 'dire_hero_2', 'dire_hero_3', 'dire_hero_4', 'dire_hero_5', 'avg_rank_tier', 'radiant_hero_1_winrate', 'radiant_hero_2_winrate', 'radiant_hero_3_winrate', 'radiant_hero_4_winrate', 'radiant_hero_5_winrate', 'dire_hero_1_winrate', 'dire_hero_2_winrate', 'dire_hero_3_winrate', 'dire_hero_4_winrate', 'dire_hero_5_winrate'])
+    # Construct the file paths
+    matches_path = os.path.join(data_parser_dir, 'matches.csv')
+    heroes_path = os.path.join(data_parser_dir, 'heroes.json')
 
-  # Add data to the dataframe using pandas library
+    # Read the CSV file
+    matches = pd.read_csv(matches_path)
+    heroes = pd.read_json(heroes_path).set_index('id')
 
-  df['match_id'] = matches['match_id']
-  df['avg_rank_tier'] = matches['avg_rank_tier']/100
-  df['radiant_win'] = matches['radiant_win'] + 0
+    df = pd.DataFrame(columns=['match_id', 'radiant_win', 'radiant_hero_1', 'radiant_hero_2', 'radiant_hero_3', 'radiant_hero_4', 'radiant_hero_5', 'dire_hero_1', 'dire_hero_2', 'dire_hero_3', 'dire_hero_4', 'dire_hero_5', 'avg_rank_tier',
+                      'radiant_hero_1_winrate', 'radiant_hero_2_winrate', 'radiant_hero_3_winrate', 'radiant_hero_4_winrate', 'radiant_hero_5_winrate', 'dire_hero_1_winrate', 'dire_hero_2_winrate', 'dire_hero_3_winrate', 'dire_hero_4_winrate', 'dire_hero_5_winrate'])
 
-  for i in range(1, 6):
-      df[f'radiant_hero_{i}'] = matches['radiant_team'].str.split(',').str[i-1]
-      df[f'dire_hero_{i}'] = matches['dire_team'].str.split(',').str[i-1]
+    # Add data to the dataframe using pandas library
 
-  # Picks and wins are stored in heroes.json file in colums 1_pick - 8_pick and 1_win - 8_win accordinally. Need to divide wins by picks to get winrate, and then add it to the dataframe
+    df['match_id'] = matches['match_id']
+    df['avg_rank_tier'] = matches['avg_rank_tier']/100
+    df['radiant_win'] = matches['radiant_win'] + 0
 
-  for i in range(1, 6):
-      df[f'radiant_hero_{i}_winrate'] = df[f'radiant_hero_{i}'].p_apply(lambda x: heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_win'] / heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_pick'])
-      df[f'dire_hero_{i}_winrate'] = df[f'dire_hero_{i}'].p_apply(lambda x: heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_win'] / heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_pick'])
+    for i in range(1, 6):
+        df[f'radiant_hero_{i}'] = matches['radiant_team'].str.split(
+            ',').str[i-1]
+        df[f'dire_hero_{i}'] = matches['dire_team'].str.split(',').str[i-1]
 
-  print(df.head())
-  # Save the dataframe to a CSV file
-  df.to_csv(path, index=False)
-  return df
+    # Picks and wins are stored in heroes.json file in colums 1_pick - 8_pick and 1_win - 8_win accordinally. Need to divide wins by picks to get winrate, and then add it to the dataframe
+
+    for i in range(1, 6):
+        df[f'radiant_hero_{i}_winrate'] = df[f'radiant_hero_{i}'].p_apply(lambda x: heroes.loc[int(
+            x)][f'{matches["avg_rank_tier"].iloc[0]//10}_win'] / heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_pick'])
+        df[f'dire_hero_{i}_winrate'] = df[f'dire_hero_{i}'].p_apply(lambda x: heroes.loc[int(
+            x)][f'{matches["avg_rank_tier"].iloc[0]//10}_win'] / heroes.loc[int(x)][f'{matches["avg_rank_tier"].iloc[0]//10}_pick'])
+
+    print(df.head())
+    # Save the dataframe to a CSV file
+    df.to_csv(path, index=False)
+    return df
--- 322bet-py/tf.py
+++ 322bet-py/tf.py
@@ -1,57 +1,75 @@
 import numpy as np
-import tensorflow as tf
 import keras
 from keras import layers
 import pandas as pd
-import os
-import torch
 from pathlib import Path
 import process_data
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 
 def get_one_hot_heroes_tensor(targets: pd.DataFrame, nb_classes: int):
+    import tensorflow as tf
+
     one_hot_mask = np.eye(nb_classes)
-    one_hot_heroes = one_hot_mask[targets[['radiant_hero_1', 'radiant_hero_2', 'radiant_hero_3', 'radiant_hero_4', 'radiant_hero_5', 'dire_hero_1', 'dire_hero_2', 'dire_hero_3', 'dire_hero_4', 'dire_hero_5']].to_numpy()]
+    one_hot_heroes = one_hot_mask[targets[['radiant_hero_1', 'radiant_hero_2', 'radiant_hero_3', 'radiant_hero_4',
+                                           'radiant_hero_5', 'dire_hero_1', 'dire_hero_2', 'dire_hero_3', 'dire_hero_4', 'dire_hero_5']].to_numpy()]
     one_hot_heroes = one_hot_heroes.reshape(len(targets), nb_classes*10)
-    one_hot_heroes_tensor = tf.convert_to_tensor(one_hot_heroes, np.float32, name='one_hot_heroes')
+    one_hot_heroes_tensor = tf.convert_to_tensor(
+        one_hot_heroes, np.float32, name='one_hot_heroes')
     return one_hot_heroes_tensor
 
+
 def main():
+    import tensorflow as tf
+
+    physical_devices = tf.config.list_physical_devices('GPU')
+    print('CONFIG', physical_devices)
+    tf.config.set_logical_device_configuration(
+        physical_devices[0], [tf.config.LogicalDeviceConfiguration(memory_limit=5600)])
+
     # Read mathes data
-    process_matches_path = os.path.join(os.getcwd(), '322bet-py','data','matches_processed.csv')
+    process_matches_path = Path('data/matches_processed.csv')
 
     if not Path(process_matches_path).exists():
         matches = process_data.process_matches(process_matches_path)
     else:
         matches = pd.read_csv(process_matches_path)
 
-    matches = matches.head(150000)
+    matches = matches.head(160000)
 
     testDataCount = len(matches)//4
-    batch_size = len(matches)*5//100
 
-    trainData  = matches.head(-testDataCount)
-    tensor_train_input = tf.convert_to_tensor(trainData[['radiant_hero_1_winrate', 'radiant_hero_2_winrate', 'radiant_hero_3_winrate', 'radiant_hero_4_winrate', 'radiant_hero_5_winrate', 'dire_hero_1_winrate', 'dire_hero_2_winrate', 'dire_hero_3_winrate', 'dire_hero_4_winrate', 'dire_hero_5_winrate', 'avg_rank_tier']].to_numpy(), np.float32, name='train_input')
+    trainData = matches.head(-testDataCount)
+    tensor_train_input = tf.convert_to_tensor(trainData[['radiant_hero_1_winrate', 'radiant_hero_2_winrate', 'radiant_hero_3_winrate', 'radiant_hero_4_winrate', 'radiant_hero_5_winrate',
+                                              'dire_hero_1_winrate', 'dire_hero_2_winrate', 'dire_hero_3_winrate', 'dire_hero_4_winrate', 'dire_hero_5_winrate', 'avg_rank_tier']].to_numpy(), np.float32, name='train_input')
     one_hot_heroes_tensor_train = get_one_hot_heroes_tensor(trainData, 140)
-    tensor_train_input = tf.concat([tensor_train_input, one_hot_heroes_tensor_train], 1)
+    # tensor_train_input = tf.concat(
+    #     [tensor_train_input, one_hot_heroes_tensor_train], 1)
     data_train_output = trainData['radiant_win'].to_numpy()
-    tensor_train_output = tf.convert_to_tensor([[i] for i in data_train_output], np.float32, name='train_output')
+    tensor_train_output = tf.convert_to_tensor(
+        [[i] for i in data_train_output], np.float32, name='train_output')
 
-    testData =  matches.tail(testDataCount)
-    tensor_test_input = tf.convert_to_tensor(testData[['radiant_hero_1_winrate', 'radiant_hero_2_winrate', 'radiant_hero_3_winrate', 'radiant_hero_4_winrate', 'radiant_hero_5_winrate', 'dire_hero_1_winrate', 'dire_hero_2_winrate', 'dire_hero_3_winrate', 'dire_hero_4_winrate', 'dire_hero_5_winrate', 'avg_rank_tier']].to_numpy(), np.float32, name='test_input')
+    testData = matches.tail(testDataCount)
+    tensor_test_input = tf.convert_to_tensor(testData[['radiant_hero_1_winrate', 'radiant_hero_2_winrate', 'radiant_hero_3_winrate', 'radiant_hero_4_winrate', 'radiant_hero_5_winrate',
+                                                       'dire_hero_1_winrate', 'dire_hero_2_winrate', 'dire_hero_3_winrate', 'dire_hero_4_winrate', 'dire_hero_5_winrate', 'avg_rank_tier']].to_numpy(), np.float32, name='test_input')
     one_hot_heroes_tensor_test = get_one_hot_heroes_tensor(testData, 140)
-    tensor_test_input = tf.concat([tensor_test_input, one_hot_heroes_tensor_test], 1)
-    data_test_output = np.array([[i] for i in testData['radiant_win'].to_numpy()])
+    # tensor_test_input = tf.concat(
+    #     [tensor_test_input, one_hot_heroes_tensor_test], 1)
+    data_test_output = np.array(
+        [[i] for i in testData['radiant_win'].to_numpy()])
 
     # Define Sequential model with 2 layers
     model = keras.Sequential(
         [
-            layers.Dense(16000, activation="relu", name="input"),
-            layers.Dense(8000, activation="relu", name="hidden1"),
-            layers.Dense(1, activation="sigmoid" ,name="output"),
+            layers.Dense(128, activation="relu", name="input"),
+            layers.Dense(64, activation="relu", name="hidden1"),
+            layers.Dense(1, activation="sigmoid", name="output"),
         ]
     )
 
-    model.compile(optimizer='adam', loss=keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
+    model.compile(optimizer='adam',
+                  loss=keras.losses.BinaryCrossentropy(), metrics=['accuracy'])
 
     checkpoint_filepath = '/tmp/checkpoint'
     model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
@@ -61,8 +79,10 @@ def main():
         mode='max',
         save_best_only=True)
 
-    print(f"Train on {len(trainData)} samples, validate on {len(testData)} samples")
-    model.fit(one_hot_heroes_tensor_train, tensor_train_output, validation_data=(one_hot_heroes_tensor_test, data_test_output), epochs=300, batch_size=batch_size, callbacks=[model_checkpoint_callback])
+    print(
+        f"Train on {len(trainData)} samples, validate on {len(testData)} samples")
+    model.fit(tensor_train_input, tensor_train_output, validation_data=(tensor_test_input,
+              data_test_output), epochs=300, batch_size=4800, callbacks=[model_checkpoint_callback])
 
     # The model weights (that are considered the best) are loaded into the
     # model.
@@ -76,8 +96,10 @@ def main():
     print(data_test_output)
 
     # Save model
-    model.save(os.path.join(os.getcwd(), '322bet-py','models','model_lts.keras'))
+    path_to_save = Path('models/model_lts.keras')
+    model.save(path_to_save)
     print("Saved model to disk")
+
 
 if __name__ == '__main__':
     main()
