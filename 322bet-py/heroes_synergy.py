09c1bb0f39d84d21edd9a1810c22c7cf56a9033d
milanjrodd <milanrodd@gmail.com>
2024-01-17 16:36:57 +0300

    refactor hero synergy calculation

--- 322bet-py/heroes_synergy.py
+++ 322bet-py/heroes_synergy.py
@@ -3,9 +3,14 @@ from gql.transport.requests import RequestsHTTPTransport
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+from parallel_pandas import ParallelPandas
+from enum import Enum
 
 from dotenv import load_dotenv
 import os
+
+# initialize parallel-pandas
+ParallelPandas.initialize(n_cpu=8, disable_pr_bar=False, split_factor=2)
 
 # Load the .env file
 load_dotenv()
@@ -13,13 +18,25 @@ load_dotenv()
 # Get the API_KEY
 API_KEY = os.getenv("STRATZ_API_KEY")
 
-# Define the RankBracketBasicEnum
-RankBracketBasicEnum = {
-    "HERALD_GUARDIAN": "HERALD_GUARDIAN",
-    "CRUSADER_ARCHON": "CRUSADER_ARCHON",
-    "LEGEND_ANCIENT": "LEGEND_ANCIENT",
-    "DIVINE_IMMORTAL": "DIVINE_IMMORTAL",
-}
+
+class RankBracketBasicEnum(Enum):
+    HERALD_GUARDIAN = 0
+    CRUSADER_ARCHON = 1
+    LEGEND_ANCIENT = 2
+    DIVINE_IMMORTAL = 3
+
+
+def map_rank_tier_to_enum(avg_rank_tier):
+    if 10 <= avg_rank_tier <= 25:
+        return RankBracketBasicEnum.HERALD_GUARDIAN
+    elif 30 <= avg_rank_tier <= 45:
+        return RankBracketBasicEnum.CRUSADER_ARCHON
+    elif 50 <= avg_rank_tier <= 65:
+        return RankBracketBasicEnum.LEGEND_ANCIENT
+    elif 70 <= avg_rank_tier <= 85:
+        return RankBracketBasicEnum.DIVINE_IMMORTAL
+    else:
+        raise ValueError("Invalid rank tier")
 
 
 def create_query(hero_ids):
@@ -61,96 +78,71 @@ transport = RequestsHTTPTransport(
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
 total_heroes = 138
-
-# Create a 3D dictionary with 138*138*4 elements
-synergyMatrix, counterMatrix = [
-    {
-        i: {
-            j: {k: 0.0 for k in range(1, total_heroes + 1)}
-            for j in range(1, total_heroes + 1)
-        }
-        for i in RankBracketBasicEnum
-    }
-    for _ in range(2)
-]
-
+batch_size = 30
 
 # Create loop for 138 heroes batched by 30 heroes in one query
-batch_size = 30
 heroes_to_fetch = range(0, total_heroes, batch_size)
 
-for i in heroes_to_fetch:
-    print(f"Executing batch {i + 1} to {min(i + batch_size, total_heroes)}")
-    hero_ids = range(i + 1, min(i + batch_size + 1, total_heroes + 1))
-    query = gql(create_query(hero_ids))
-    for rank in RankBracketBasicEnum:
-        result = client.execute(query, {"mmr": rank})
-        print(f"{rank} executed for heroes {hero_ids}")
 
-        for k, hero_id in enumerate(hero_ids, start=1):
-            advantages = result["heroStats"][f"h{k}"]["advantage"]
+def ApplySynergyData(matches_data: pd.DataFrame):
+    """Apply Synergy Data"""
+    # Create a 4D numpy array with dimensions (total_ranks, total_heroes, total_heroes)
+    synergyMatrix, counterMatrix = [
+        np.zeros((len(RankBracketBasicEnum), total_heroes + 1, total_heroes + 1))
+        for _ in range(2)
+    ]
 
-            for l, advantage in enumerate(advantages):
-                synergiesWith = {
-                    advantage_with["heroId2"]: advantage_with["synergy"] / 100
-                    for advantage_with in advantage["with"]
-                }
-                synergiesVs = {
-                    advantage_vs["heroId2"]: advantage_vs["synergy"] / 100
-                    for advantage_vs in advantage["vs"]
-                }
-
-                for hero_id2 in synergiesWith.keys():
-                    synergyMatrix[rank][hero_id][hero_id2] = synergiesWith[hero_id2]
-
-                for hero_id2 in synergiesVs.keys():
-                    counterMatrix[rank][hero_id][hero_id2] = synergiesVs[hero_id2]
-
-# create dataframe from dictionary synergyMatrix["HERALD_GUARDIAN"]
-
-
-# call the function (matches_data)
-def CreateSynergyData(matches_data: pd.DataFrame):
-    output = []
-
-    for _, match in matches_data.iterrows():
+    def calculate_synergy(match: pd.Series):
         data = []
-
-        rankTier = match["avg_rank_tier"].astype(float) * 100
-
-        if rankTier >= 10 and rankTier <= 25:
-            rankTier = "HERALD_GUARDIAN"
-        elif rankTier >= 30 and rankTier <= 45:
-            rankTier = "CRUSADER_ARCHON"
-        elif rankTier >= 50 and rankTier <= 65:
-            rankTier = "LEGEND_ANCIENT"
-        elif rankTier >= 70 and rankTier <= 85:
-            rankTier = "DIVINE_IMMORTAL"
-        else:
-            raise Exception("Invalid rank tier")
+        rank = map_rank_tier_to_enum(match["avg_rank_tier"].astype(float) * 100)
 
         for hero1 in range(1, 6):
-            radiantHero1 = match[f"radiant_hero_{hero1}"]
-            direHero1 = match[f"dire_hero_{hero1}"]
+            radiantHero1 = match[f"radiant_hero_{hero1}"].astype(int)
+            direHero1 = match[f"dire_hero_{hero1}"].astype(int)
             for hero2 in range(1, 6):
+                direHero2 = match[f"dire_hero_{hero2}"].astype(int)
+                data.append(counterMatrix[rank.value][radiantHero1][direHero2])
+
                 if hero1 == hero2:
                     continue
 
-                radiantHero2 = match[f"radiant_hero_{hero2}"]
-                direHero2 = match[f"dire_hero_{hero2}"]
+                radiantHero2 = match[f"radiant_hero_{hero2}"].astype(int)
 
-                data.append(synergyMatrix[rankTier][radiantHero1][radiantHero2])
-                data.append(synergyMatrix[rankTier][direHero1][direHero2])
+                data.append(synergyMatrix[rank.value][radiantHero1][radiantHero2])
+                data.append(synergyMatrix[rank.value][direHero1][direHero2])
 
-            for hero2 in range(1, 6):
-                direHero2 = match[f"dire_hero_{hero2}"]
-                data.append(counterMatrix[rankTier][radiantHero1][direHero2])
+        return data
 
-        output.append(data)
+    for i in heroes_to_fetch:
+        print(f"Executing batch {i + 1} to {min(i + batch_size, total_heroes)}")
+        hero_ids = range(i + 1, min(i + batch_size + 1, total_heroes + 1))
+        query = gql(create_query(hero_ids))
+        for rank in RankBracketBasicEnum:
+            result = client.execute(query, {"mmr": rank.name})
+            print(f"{rank} executed for heroes {hero_ids}")
 
-    return output
+            for k, hero_id in enumerate(hero_ids, start=1):
+                advantages = result["heroStats"][f"h{k}"]["advantage"]
 
+                for l, advantage in enumerate(advantages):
+                    synergiesWith = {
+                        advantage_with["heroId2"]: advantage_with["synergy"] / 100
+                        for advantage_with in advantage["with"]
+                    }
+                    synergiesVs = {
+                        advantage_vs["heroId2"]: advantage_vs["synergy"] / 100
+                        for advantage_vs in advantage["vs"]
+                    }
 
-# for rank in RankBracketBasicEnum:
-#     df = pd.DataFrame.from_dict(synergyMatrix[rank], orient="index")
-#     print(f"{rank} MATRIX\n", df)
+                    for hero_id2 in synergiesWith.keys():
+                        synergyMatrix[rank.value][hero_id][hero_id2] = synergiesWith[
+                            hero_id2
+                        ]
+
+                    for hero_id2 in synergiesVs.keys():
+                        counterMatrix[rank.value][hero_id][hero_id2] = synergiesVs[
+                            hero_id2
+                        ]
+
+    output = matches_data.p_apply(calculate_synergy, axis=1)  # type: ignore
+    return output.values.tolist()
